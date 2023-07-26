8295765788238dd791d301c14785f4ab0b42f2a8
Emha "Fudhol" Ismaulidin <72345462+EmhaFudholIsmaulidin@users.noreply.github.com>
2023-07-26 13:41:32 +0800

    Day 2 of Magang di Fairtech.
    Saya sedang mengerjakan slicing web. Ini untuk memenuhi latihan pertama.

---
+++ Project/resources/script.js
@@ -0,0 +1,58 @@
+function dropdown() {
+  var x = document.getElementById("dropdown-content");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  } 
+}
+
+let rapperList = [];
+let currentRapperIndex = 0;
+
+function loadDoc() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    rapperList = this.responseText.split("\n").filter(Boolean);
+    loadNextRapper();
+  }
+  xhttp.open("GET", "resources/rapper.txt");
+  xhttp.send();
+}
+
+function loadNextRapper() {
+  if (currentRapperIndex >= rapperList.length) {
+    currentRapperIndex = 0;
+  }
+
+  const rapperName = rapperList[currentRapperIndex];
+  document.getElementById("namaRapper").innerHTML = rapperName;
+
+  currentRapperIndex++;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  loadDoc();
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
