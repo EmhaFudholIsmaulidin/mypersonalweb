b992be16107ab90cceada56be836a9dd9fad6606
Emha Ismaulidin <cbzkumala@gmail.com>
2023-08-02 11:42:47 +0800

    Update sejauh hari 8. AJAX belum berfungsi.

--- resources/script.js
+++ resources/script.js
@@ -1,28 +1,24 @@
-let rapperList = [
-    "http://api.alquran.cloud/v1/ayah/262"
-];
-let currentRapperIndex = 0;
+$(document).ready(function(){
+  $("button").click(function(){
+    var randomAyahNumber = Math.floor(Math.random() * 6236) + 1;
+    var apiUrl = "http://api.alquran.cloud/v1/ayah/" + randomAyahNumber;
 
-function iterateSong() {
-  if (currentRapperIndex >= rapperList.length) {
-    currentRapperIndex = 0;
-  }
+    $.ajax({
+      url: apiUrl,
+      type: "GET",
+      dataType: "json",
+      success: function(data) {
+        var ayahText = data.data.text;
+        var ayahTranslation = data.data.translation;
 
-  const rapperName = rapperList[currentRapperIndex];
-  document.getElementById("embedSpotify").src = rapperName;
-
-  currentRapperIndex++;
-}
-
-var settings = {
-  "url": "http://api.alquran.cloud/v1/ayah/262/en.asad",
-  "method": "GET",
-  "timeout": 0,
-};
-
-$.ajax(settings).done(function (response) {
-  const juz = response.data.juz;
-  console.log(juz)
+        var ayahDataDiv = $("#quran");
+        ayahDataDiv.html("<h1>" + ayahText + "</h1><p>" + ayahTranslation + "</p>");
+      },
+      error: function() {
+        alert("Error occurred while fetching data.");
+      }
+    });
+  });
 });
 
 let slideIndex = 1;
@@ -48,51 +44,20 @@ function showSlides(n) {
 }
 
 $(document).ready(function() {
+  
   $(".btt").click(function() {
       $(document).scrollTop(0);
   });
+
+  $(".column img").hover(function(){
+    $(this).css({"filter": "grayscale(0%)blur(0px)"});
+    $(this).parent().css({"max-width": "37%"});
+    $(this).parent().siblings().css({"max-width": "31%"});
+  }, function(){
+    $(this).css({"filter": "grayscale(100%)blur(1px)"});
+    $(this).parent().css({"max-width": "33%"});
+    $(this).parent().siblings().css({"max-width": "33%"});
+  });
+
+  
 });
-
-// // Get the modal
-// var modal = document.getElementById("myModal");
-
-// // Get the image and insert it inside the modal - use its "alt" text as a caption
-// var img = document.getElementById("myImg");
-// var modalImg = document.getElementById("img01");
-// var captionText = document.getElementById("caption");
-// img.onclick = function(){
-//   modal.style.display = "block";
-//   modalImg.src = this.src;
-//   captionText.innerHTML = this.alt;
-// }
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() { 
-//   modal.style.display = "none";
-// }
-
-let tombolgue = document.getElementById("tbl");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    setTimeout(function(){
-      tombolgue.style.display = "block";
-    },0);
-  } else {
-    setTimeout(function(){
-      tombolgue.style.display = "none";
-    },0);
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
